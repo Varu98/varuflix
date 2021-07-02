ab30e07a96fe4638d0b7689019e68c94b2a0fcf6
Vardaan <vardaan.agarwal.98@gmail.com>
2021-07-02 17:43:41 +0530

    varuflix banner and body upload

---
+++ src/Row.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Rows.css";
+
+const base_url = "https://image.tmdb.org/t/p/w300";
+// const api_KEY = "f4bf30038595da0ba4600087ce09a5e9";
+
+function Row({ title, fetchURL }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchURL]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row_poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
